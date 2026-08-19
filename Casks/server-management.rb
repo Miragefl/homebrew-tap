b78b4794c57bf9360e9fcf915164366a0d5c0a29
fenglei <silence_fl@yeah.net>
2026-08-19 19:14:21 +0800

    server-management 0.3.7

--- Casks/server-management.rb
+++ Casks/server-management.rb
@@ -1,8 +1,8 @@
 cask "server-management" do
-  version "0.3.6"
-  sha256 "058ab40842f7ccf25776a608d70b7b55bb39aeed1b7baf181d7953a45b7d6467"
+  version "0.3.7"
+  sha256 "12ae82a3a9cb514357079c49b817f5daef918c4b1a5ab70b06c0b3be6cc4ee21"
 
-  url "https://github.com/Miragefl/server-management/releases/download/v0.3.6/ServerManagement-v0.3.6.zip",
+  url "https://github.com/Miragefl/server-management/releases/download/v0.3.7/ServerManagement-v0.3.7.zip",
       verified: "github.com/Miragefl/server-management/"
   name "Server Management"
   desc "macOS 服务器信息管理工具：服务器/服务/凭据记录"
